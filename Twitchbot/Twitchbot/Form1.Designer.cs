ca0d4c4af2706455d483cb765fc8dc1aa1b4f1d6
Roobert Björkbacka <Roobert Bjrkbacka>
2016-04-05 16:46:28 +0300

    first commit

---
+++ Twitchbot/Twitchbot/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Twitchbot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.Olive;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Maiandra GD", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Maroon;
+            this.textBox1.HideSelection = false;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(835, 430);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.SystemColors.InfoText;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.ForeColor = System.Drawing.Color.Lime;
+            this.textBox2.Location = new System.Drawing.Point(12, 453);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(292, 93);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Papyrus", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(310, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 88);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(859, 553);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Rantbot";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Twitchbot/Twitchbot/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Twitchbot
+{
+    public partial class Form1 : Form
+    {
+        Queue<string> sendMessageQueue;
+        TcpClient Client;
+        NetworkStream netstream;
+        StreamReader reader;
+        DateTime lastMessage;
+        StreamWriter writer;
+        string username, password,channelname,trimchat;
+
+        public Form1()
+        {
+            sendMessageQueue = new Queue<string>();
+            this.username = "rantbott";
+            this.channelname = "ropzelcius";
+            this.password = File.ReadAllText("password.txt");
+            this.trimchat = $":{username}!{username}@{username}.tmi.twitch.tv PRIVMSG #{channelname} :";
+            InitializeComponent();
+            Login();
+            lastMessage = DateTime.Now;
+        }
+        public void Login()
+        {
+            Client = new TcpClient("irc.twitch.tv", 6667);
+            netstream = Client.GetStream();
+            reader = new StreamReader(netstream, Encoding.GetEncoding("iso8859-1"));
+            writer = new StreamWriter(netstream, Encoding.GetEncoding("iso8859-1"));
+            writer.WriteLine("PASS " + password);
+            writer.Flush();
+            writer.WriteLine("USER " + username + " 8 * :" + username);
+            writer.Flush();
+            writer.WriteLine("NICK " + username);
+            writer.Flush();
+            writer.WriteLine("JOIN #"+channelname);
+            writer.Flush();
+            textBox1.Text += Environment.NewLine + "Connected";
+            SendMessage("RantBot Connected, Chat is gonna be deleted now #RANTSI");
+
+
+        }
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            
+            if (!Client.Connected)
+            {
+                Login();
+            }
+            
+                TryReceiveMessages();
+                TrySendingMessages();
+            
+
+        }
+        public void SendMessage(string message)
+        {
+            sendMessageQueue.Enqueue(message);
+            
+        }
+
+
+        void ReceiveMessage(string nick, string message,string channel,string type,string trimchat)
+        {
+            if (type == "PRIVMSG" && channel.Contains("#"))
+            {
+                textBox1.Text += Environment.NewLine + channel + "<" + DateTime.Now.ToString("HH:mm:ss") + "> <" + nick + "> " + message;
+                textBox1.SelectionStart = textBox1.Text.Length;
+                textBox1.ScrollToCaret();
+            }
+            if(message.StartsWith("!hi"))
+            {
+                SendMessage($"Hello, {nick}");
+            }
+
+        }
+
+
+        void TryReceiveMessages()
+        {
+            if (Client.Available >0 )
+            {
+                string data = "";
+                data = reader.ReadLine();
+                string nick = "";
+                string type = "";
+                string channel = "";
+                string message = "";
+                string[] split1 = data.Split(':');
+                string[] ex;
+                ex = data.Split(new char[] { ' ' }, 5);
+                if (ex[0] == "PING")
+                {
+                    SendMessage("PONG " + ex[1]);
+                }
+
+                if (data.Contains("PRIVMSG"))
+                {
+                    if (split1.Length > 1)
+                    {
+                        string[] split2 = split1[1].Split(' ');
+                        nick = split2[0];
+                        nick = nick.Split('!')[0];
+                        type = split2[1]; //Miksi tämä kaatuu tässä???????????????????????????
+                        channel = split2[2];
+                        if (split1.Length > 2)
+                        {
+                            for (int i = 2; i < split1.Length; i++)
+                            {
+                                message += split1[i] + " ";
+                            }
+                        }
+                        trimchat= $":{nick}!{nick}@{nick}.tmi.twitch.tv PRIVMSG #{channelname} :";
+                    }
+                }
+
+                ReceiveMessage(nick, message, channel, type,trimchat);
+            }
+        }
+        void TrySendingMessages()
+        {
+            if(DateTime.Now-lastMessage>TimeSpan.FromSeconds(2))
+            {
+                if (sendMessageQueue.Count > 0)
+                {
+                    var message = sendMessageQueue.Dequeue();
+                    writer.WriteLine($"{trimchat}{message}");
+                    writer.Flush();
+                    lastMessage = DateTime.Now;
+                }
+
+            }
+            
+
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendMessage(textBox2.Text);
+        }
+
+        
+    }
+}
